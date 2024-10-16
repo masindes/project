a07ec2f0c20a1235eb291c7c24485ef73f2c2f33
masindes <maasinde.sylvester@gmail.com>
2024-10-16 11:35:01 +0300

    update app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const apiKey = "7e6add09d1fa415088ac94d6924e3f1b"
+
+const blogContainer = document.getElementById("blogContainer");
+
+function fechRandomNews(){
+    try{
+
+    } catch(e){
+        console.error("Error fetching news", e);
+        return[]
+    }
+}
